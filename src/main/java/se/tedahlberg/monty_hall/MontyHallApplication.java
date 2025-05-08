054d9b77b5be155aeab79c18eeba159bfeed7137
Ted Ahlberg <ted.ahlberg95@gmail.com>
2025-05-08 16:19:35 +0200

    chore: Initial commit

---
+++ src/main/java/se/tedahlberg/monty_hall/MontyHallApplication.java
@@ -0,0 +1,13 @@
+package se.tedahlberg.monty_hall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontyHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontyHallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/tedahlberg/monty_hall/MontyHallApplicationTests.java
@@ -0,0 +1,13 @@
+package se.tedahlberg.monty_hall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontyHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
