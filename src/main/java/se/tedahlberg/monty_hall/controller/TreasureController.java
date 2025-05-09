0d6eda991ea190238d2fc4312f03062a58a952e1
Ted Ahlberg <ted.ahlberg95@gmail.com>
2025-05-09 11:29:40 +0200

    feat: Integration tests for enpoint `treasure/find`

--- src/main/java/se/tedahlberg/monty_hall/controller/TreasureController.java
+++ src/main/java/se/tedahlberg/monty_hall/controller/TreasureController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.tedahlberg.monty_hall.service.TreasureService;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,13 +15,10 @@ public class TreasureController {
   private final TreasureService treasureService = new TreasureService();
 
   @GetMapping("/find")
-  public String findTreasure(
-      @RequestParam(name = "trustJack", required = false, defaultValue = "true") Boolean trustJack) {
+  public ResponseEntity<Boolean> findTreasure(
+      @RequestParam(required = false, defaultValue = "true") Boolean trustJack) {
     Boolean foundTreasure = treasureService.tryFindingTreasure(trustJack);
-    if (foundTreasure) {
-      return "You found the treasure!";
-    } else {
-      return "No treasure found.";
-    }
+
+    return ResponseEntity.ok(foundTreasure);
   }
 }
--- src/test/java/se/tedahlberg/monty_hall/MontyHallApplicationTests.java
+++ src/test/java/se/tedahlberg/monty_hall/MontyHallApplicationTests.java
@@ -1,13 +1,90 @@
 package se.tedahlberg.monty_hall;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import se.tedahlberg.monty_hall.controller.TreasureController;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MontyHallApplicationTests {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TreasureController treasureController;
+
+	private TestRestTemplate restTemplate = new TestRestTemplate();
+	private HttpHeaders headers = new HttpHeaders();
+
+	private void println(String message) {
+		System.out.println("[" + ANSI_GREEN + "TEST" + ANSI_RESET + "] " + message);
 	}
 
+	private String createUrl(String path) {
+		return "http://localhost:" + port + path;
+	}
+
+	private String calculateTreasureFindingPercentage(Boolean trustJack) {
+		HttpEntity<Boolean> entity = new HttpEntity<Boolean>(null, headers);
+
+		int numberOfTries = 1000;
+		int numberOfTreasures = 0;
+
+		for (int i = 0; i < numberOfTries; i++) {
+			ResponseEntity<Boolean> response = restTemplate.exchange(createUrl(
+					"/treasure/find?trustJack=" + trustJack), HttpMethod.GET, entity, Boolean.class);
+
+			final Boolean foundTreasure = response.getBody();
+
+			if (foundTreasure) {
+				numberOfTreasures++;
+			}
+		}
+
+		double percentage = (double) numberOfTreasures / numberOfTries * 100;
+		return String.format("%.2f", percentage) + "%";
+	}
+
+	@Test
+	@Order(1)
+	void contextLoads() {
+		println("Context loads");
+		assertThat(treasureController).isNotNull();
+	}
+
+	@Test
+	@Order(2)
+	void percentageOfTreasuresWhenTrustingJack() {
+		println("Percentage of treasures found when trusting Jack");
+
+		String percentage = calculateTreasureFindingPercentage(true);
+
+		println("Precentage of treasures found: " + percentage);
+	}
+
+	@Test
+	@Order(3)
+	void percentageOfTreasuresWhenNotTrustingJack() {
+		println("Percentage of treasures found when not trusting Jack");
+
+		String percentage = calculateTreasureFindingPercentage(false);
+
+		println("Precentage of treasures found: " + percentage);
+	}
 }
